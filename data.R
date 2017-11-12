c4817d9ace377cf515f89c09f43e51ade221fb71
profbiscuit <matthew.buras@yahoo.com>
2017-11-11 23:53:44 -0700

    Split into reg and post season. Commented out diagnostic code and added a vector of player id numbers (1:10)

--- data.R
+++ data.R
@@ -11,12 +11,14 @@ packages <- c("rvest", "stringr", "tidyr")
 
 ipak(packages)
 
+player <- 1:10
+
 url <- 'http://games.espn.com/ffl/schedule?leagueId=130349&teamId=2'
 webpage <- read_html(url)
 
 sb_table <- html_nodes(webpage, 'table')
 sb <- html_table(sb_table, fill = TRUE)[[1]]
-head(sb)
+# head(sb)
 
 sb1 <- sb[3:36, c(1, 2, 4, 5)]
 sb1[1, 1] <- "Week"
@@ -32,4 +34,13 @@ sb2$Result <- gsub("-", " ", sb2$Result)
 sb2$Opponent <- substr(sb2$Opponent, 1, nchar(sb2$Opponent)-5)
 
 sb3 <- separate(sb2, Result, c('Result', 'PF', 'PA'), sep = ' ', remove = TRUE)
-head(sb3)
+sb3$PF <- as.numeric(sb3$PF)
+sb3$PA <- as.numeric(sb3$PA)
+
+# head(sb3, 13)
+
+reg <- sb3[1:13, ]
+reg
+
+post <- sb3[14:16, ]
+post
