3cbcf507d23ba34ca982bf3f3a7270f24ddf0f4e
profbiscuit <matthew.buras@yahoo.com>
2017-11-11 23:39:18 -0700

    Finished cleaning first scrapped table

--- data.R
+++ data.R
@@ -1,12 +1,15 @@
 # https://rpubs.com/Radcliffe/superbowl
 
-install.packages('rvest')
-install.packages('stringr')
-install.packages('tidyr')
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
 
-library(rvest)
-library(stringr)
-library(tidyr)
+packages <- c("rvest", "stringr", "tidyr")
+
+ipak(packages)
 
 url <- 'http://games.espn.com/ffl/schedule?leagueId=130349&teamId=2'
 webpage <- read_html(url)
@@ -15,6 +18,18 @@ sb_table <- html_nodes(webpage, 'table')
 sb <- html_table(sb_table, fill = TRUE)[[1]]
 head(sb)
 
-sb1 <- sb[3:36, 1:5]
-names(sb1)[1] <- "Week"
-names(sb1)
+sb1 <- sb[3:36, c(1, 2, 4, 5)]
+sb1[1, 1] <- "Week"
+names(sb1) <- c('Week', 'Result', 'Opponent', 'Owner')
+
+sb2 <- sb1[-1, ]
+
+sb2$Result <- gsub("-", " ", sb2$Result)
+
+#sb2$Record <- substr(sb2$Opponent, nchar(sb2$Opponent)-5+1, nchar(sb2$Opponent))
+#sb2$Record <- substr(sb2$Record, 2, 4)
+
+sb2$Opponent <- substr(sb2$Opponent, 1, nchar(sb2$Opponent)-5)
+
+sb3 <- separate(sb2, Result, c('Result', 'PF', 'PA'), sep = ' ', remove = TRUE)
+head(sb3)
