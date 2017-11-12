c9bbb8e8e1f6f3b33c423a7175f23eb573792134
profbiscuit <matthew.buras@yahoo.com>
2017-11-12 08:51:45 -0700

    added loop to generate the player urls for their schedule page.
    cleanify function still in disrepair

--- data.R
+++ data.R
@@ -14,7 +14,53 @@ ipak(packages)
 player <- 1:10
 
 url <- 'http://games.espn.com/ffl/schedule?leagueId=130349&teamId=2'
+
 webpage <- read_html(url)
+
+sb_table <- html_nodes(webpage, 'table')
+sb <- html_table(sb_table, fill = TRUE)[[1]]
+# head(sb)
+
+sb1 <- sb[3:36, c(1, 2, 4, 5)]
+sb1[1, 1] <- "Week"
+names(sb1) <- c('Week', 'Result', 'Opponent', 'Owner')
+
+sb2 <- sb1[-1, ]
+
+sb2$Result <- gsub("-", " ", sb2$Result)
+
+#sb2$Record <- substr(sb2$Opponent, nchar(sb2$Opponent)-5+1, nchar(sb2$Opponent))
+#sb2$Record <- substr(sb2$Record, 2, 4)
+
+sb2$Opponent <- substr(sb2$Opponent, 1, nchar(sb2$Opponent)-5)
+
+sb3 <- separate(sb2, Result, c('Result', 'PF', 'PA'), sep = ' ', remove = TRUE)
+sb3$PF <- as.numeric(sb3$PF)
+sb3$PA <- as.numeric(sb3$PA)
+
+# head(sb3, 13)
+
+reg <- sb3[1:13, ]
+reg
+
+post <- sb3[14:16, ]
+post
+
+############ Others
+
+urls <- rep(0, 10)
+
+player <- 1:10
+
+for (i in seq_along(player)){
+  urls[[i]] <- paste0("http://games.espn.com/ffl/schedule?leagueId=130349&teamId=", i)
+}
+
+urls <- as.data.frame(url, stringsAsFactors = FALSE)
+
+url_pull <- urls[1]
+
+webpage <- read_html(url_pull)
 
 sb_table <- html_nodes(webpage, 'table')
 sb <- html_table(sb_table, fill = TRUE)[[1]]
@@ -51,7 +97,7 @@ post
 
 cleanify <- function(url){
   
-  webpage <- read_html(url)
+  webpage <- read_html(substitute(url))
 
   sb_table <- html_nodes(webpage, 'table')
   sb <- html_table(sb_table, fill = TRUE)[[1]]
@@ -74,4 +120,5 @@ cleanify <- function(url){
   
   rm(webpage, sb_table, sb, sb1, sb2, sb3)
 }
+
 
