6d1d4bdd3f898f4242a47983499688049dc097ec
profbiscuit <matthew.buras@yahoo.com>
2017-11-12 00:06:16 -0700

    Functionalized the scraping procedure for a given url.

--- data.R
+++ data.R
@@ -44,3 +44,34 @@ reg
 
 post <- sb3[14:16, ]
 post
+
+################### Draft Above Into a Function ###################
+
+# might have to add column with player id
+
+cleanify <- function(url){
+  webpage <- read_html(url)
+
+  sb_table <- html_nodes(webpage, 'table')
+  sb <- html_table(sb_table, fill = TRUE)[[1]]
+  
+  sb1 <- sb[3:36, c(1, 2, 4, 5)]
+  sb1[1, 1] <- "Week"
+  names(sb1) <- c('Week', 'Result', 'Opponent', 'Owner')
+  
+  sb2 <- sb1[-1, ]
+  
+  sb2$Result <- gsub("-", " ", sb2$Result)
+  
+  sb2$Opponent <- substr(sb2$Opponent, 1, nchar(sb2$Opponent)-5)
+  
+  sb3 <- suppressWarnings(separate(sb2, Result, c('Result', 'PF', 'PA'), sep = ' ', remove = TRUE))
+  sb3$PF <- as.numeric(sb3$PF)
+  sb3$PA <- as.numeric(sb3$PA)
+  
+  reg <- sb3[1:13, ]
+  
+  post <- sb3[14:16, ]
+  
+  rm(sb, sb1, sb2, sb3)
+}
